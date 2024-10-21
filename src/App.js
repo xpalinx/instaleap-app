2da15b8b2d1a5b2c9df3d3cb6992a3bf37b8203e
Pablo Veintemilla <pbr.9816@gmail.com>
2024-10-21 17:23:17 -0500

    added src

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
         headers: {
           accept: 'application/json',
           'content-type': 'application/json',
-          'x-api-key': 'yoJYongi4V4m0S4LClubdyiu5nq6VIpxazcFaghi',
+          'x-api-key': '',
         },
       });
   
@@ -135,7 +135,7 @@ function App() {
   const fetchLocationDetails = async (lat, lng) => {
     try {
       const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyCgmf6MnqMcNX3y6mlKE3EEb8tdgwFM7KY`
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=`
       );
       const addressComponents = response.data.results[0].address_components;
       const formattedAddress = response.data.results[0].formatted_address;
@@ -172,7 +172,7 @@ function App() {
       <button onClick={handleCheckout}>Checkout</button>
 
       {/* Google Maps */}
-      <LoadScript googleMapsApiKey="AIzaSyCgmf6MnqMcNX3y6mlKE3EEb8tdgwFM7KY">
+      <LoadScript googleMapsApiKey="">
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           center={mapCenter}
