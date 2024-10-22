b46647350f1f3d36d3ff65b7c88e1c85b2aeed92
Pablo Veintemilla <pbr.9816@gmail.com>
2024-10-21 20:05:08 -0500

    Delete src/config directory

--- src/config/credentials.js
+++
@@ -1,8 +0,0 @@
-// config/credentials.js
-const credentials = {
-    apiKey: 'yoJYongi4V4m0S4LClubdyiu5nq6VIpxazcFaghi',
-    mapskey: 'AIzaSyCgmf6MnqMcNX3y6mlKE3EEb8tdgwFM7KY',
-  };
-  
-export default credentials;
-  
